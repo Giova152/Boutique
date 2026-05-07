1ba255a2a1ac342c52002bc14dca1add365f3386
Giovanny Gandonou <midogiova@gmail.com>
2026-05-07 06:41:13 +0100

    Add code splitting and SPA rewrite for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          stripe: ['@stripe/stripe-js', '@stripe/react-stripe-js'],
+          paypal: ['@paypal/react-paypal-js'],
+        }
+      }
+    }
+  },
   server: {
     headers: {
       'X-Content-Type-Options': 'nosniff',
